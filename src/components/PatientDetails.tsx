6d18b989673cd1e2f061337151e8937e55de606e
Daniel Angel <daniel.reverol@gmail.com>
2024-05-14 09:59:03 +0100

    Patients list

---
+++ src/components/PatientDetails.tsx
@@ -0,0 +1,10 @@
+import { Patient } from "../types";
+
+type PatientDetailsProps = {
+  patient: Patient
+}
+export default function PatientDetails({patient} : PatientDetailsProps) {
+  return (
+    <div>{patient.name}</div>
+  )
+}
--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,7 +1,34 @@
 import { usePatientStore } from "../store";
+import PatientDetails from "./PatientDetails";
 
 export default function PatientsList() {
   const { patients } = usePatientStore();
-  console.log("🚀 ~ PatientsList ~ patients:", patients);
-  return <div>PatientsList</div>;
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      {patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Patients list</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Sort your{" "}
+            <span className="text-indigo-600 font-bold">
+              Patients and Appointments
+            </span>
+          </p>
+          {patients.map((patient) => (
+            <PatientDetails key={patient.id} patient={patient} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No patients</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Start adding patients{" "}
+            <span className="text-indigo-600 font-bold">
+              and they will appear here
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
 }
